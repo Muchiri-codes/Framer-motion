8a11d33f6cbbaf06945940bf9a26763734e01c5a
John Muchiri <muchirijohn0018@gmail.com>
2025-12-27 23:10:20 +0300

    first commit in the framer motion tut

---
+++ src/App.js
@@ -0,0 +1,40 @@
+import React, { useState } from 'react';
+import { BrowserRouter, Route, Routes } from "react-router-dom"; // Added BrowserRouter
+import Header from './components/Header';
+import Home from './components/Home';
+import Base from './components/base';
+import Toppings from './components/Toppings';
+import Order from './components/Order.js';
+
+function App() {
+  const [pizza, setPizza] = useState({ base: "", toppings: [] });
+
+  const addBase = (base) => {
+    setPizza({ ...pizza, base })
+  }
+  
+  const addTopping = (topping) => {
+    let newToppings;
+    if(!pizza.toppings.includes(topping)){
+      newToppings = [...pizza.toppings, topping];
+    } else {
+      newToppings = pizza.toppings.filter(item => item !== topping);
+    }
+    setPizza({ ...pizza, toppings: newToppings });
+  }
+
+  return (
+    <BrowserRouter> {/* Wrap everything in BrowserRouter */}
+      <Header />
+      <Routes>
+        {/* Use element prop instead of children */}
+        <Route path="/base" element={<Base addBase={addBase} pizza={pizza} />} />
+        <Route path="/toppings" element={<Toppings addTopping={addTopping} pizza={pizza} />} />
+        <Route path="/order" element={<Order pizza={pizza} />} />
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
