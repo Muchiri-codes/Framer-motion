d4afcc8fff762404ede69afbafa9397bed3948fb
John Muchiri <muchirijohn0018@gmail.com>
2025-12-28 19:34:07 +0300

    All about variants and orchestration properties such as stagger and when

--- src/components/base.js
+++ src/components/base.js
@@ -1,27 +1,59 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import {motion } from 'framer-motion';
+import { motion } from 'framer-motion';
+
+
+
+//variants helps define the animation parameters outside of the element thus keeping them neater
+const containerVariants = {
+  hidden: {
+    opacity: 0,
+    x: '100vw'
+  },
+  visible: {
+    opacity: 1,
+    x: 0,
+    transition: {
+      type: 'spring',
+      delay: 0.5
+    }
+  }
+}
+
+
+const nextVariants = {
+  hidden: {
+    x: '-100vw'
+  },
+  visible: {
+    x: 0,
+    transition: {
+      type: 'spring',
+      stiffness: 120
+    }
+  }
+}
 
 const Base = ({ addBase, pizza }) => {
   const bases = ['Classic', 'Thin & Crispy', 'Thick Crust'];
 
   return (
     <motion.div className="base container"
-    initial={{x:'100vw'}}
-    animate={{x:0}}
-    transition={{type:'spring', delay:0.5}}
+      variants={containerVariants}
+      initial="hidden"
+      animate="visible"
     >
 
       <h3>Step 1: Choose Your Base</h3>
       <ul>
         {bases.map(base => {
-          let spanClass = pizza.base === base ? 'active' : '';
+          let spanClass = pizza.base === base ? 'active' : null;
           return (
             <motion.li key={base} onClick={() => addBase(base)}
-            whileHover={{scale:1.3, originX:0, color:'#f8e112'}}
-            transition={{type:'spring', stiffness:300}}
+              whileHover={{ scale: 1.3, originX: 0, color: '#f8e112' }}
+              transition={{ type: 'spring', stiffness: 300 }}
             >
-              <span className={spanClass}>{ base }</span>
+              <span className={spanClass}>{base}</span>
             </motion.li>
           )
         })}
@@ -29,17 +61,17 @@ const Base = ({ addBase, pizza }) => {
 
       {pizza.base && (
         <motion.div className="next"
-        initial= {{x:'-100vw'}}
-        animate={{x:0}}
-        transition={{type:'spring', stiffness:120}}
+          variants={nextVariants}
+          initial="hidden"
+          animate="visible"
         >
           <Link to="/toppings">
             <motion.button
-            whileHover={{
-              scale:1.2,
-              textShadow:"0px 0px 8px rgb(255,255,255)",
-              boxShadow:"0px 0px 8px rgb(255,255,255)"
-            }}
+              whileHover={{
+                scale: 1.2,
+                textShadow: "0px 0px 8px rgb(255,255,255)",
+                boxShadow: "0px 0px 8px rgb(255,255,255)"
+              }}
             >Next</motion.button>
           </Link>
         </motion.div>
